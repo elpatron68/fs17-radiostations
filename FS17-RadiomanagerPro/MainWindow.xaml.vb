99f31610bfd36bef2d2ba15c24f4e9ae55d24c79
markus <m.busche@medisoftware.de>
2017-05-21 16:07:58 +0200

    First commit

---
+++ FS17-RadiomanagerPro/MainWindow.xaml.vb
@@ -0,0 +1,55 @@
+﻿Imports System.IO
+Imports System.Net
+Imports Newtonsoft.Json
+Class MainWindow
+    Private Sub btnSearch_Click(sender As Object, e As RoutedEventArgs) Handles btnSearch.Click
+        Dim url = "http://www.radio-browser.info/webservice/json/stations/byname/" + textBox.Text
+        Dim request As HttpWebRequest = CType(WebRequest.Create(url), HttpWebRequest)
+        request.Method = WebRequestMethods.Http.[Get]
+        request.UserAgent = "FS17-Radiomanager/1.0"
+        request.ContentType = "application/json; charset=utf-8"
+
+        Dim sJsonAnswer As String
+        Dim response = DirectCast(request.GetResponse(), HttpWebResponse)
+
+        Using sr = New StreamReader(response.GetResponseStream())
+            sJsonAnswer = sr.ReadToEnd()
+        End Using
+        Dim Radiostations As List(Of Searchresult) = DirectCast(JsonConvert.DeserializeObject(sJsonAnswer, GetType(List(Of Searchresult))),
+            List(Of Searchresult))
+        Dim view As CollectionView = CollectionViewSource.GetDefaultView(Radiostations)
+        ' view.GroupDescriptions.Add(New PropertyGroupDescription("Name"))
+        resultView.ItemsSource = view
+    End Sub
+
+    Private Sub _AddStation(sender As Object, e As RoutedEventArgs)
+        Dim url As String = DirectCast(DirectCast(e.Source, MenuItem).CommandParameter, Searchresult).url
+        Dim sStationName As String = DirectCast(DirectCast(e.Source, MenuItem).CommandParameter, Searchresult).name
+        _AddToFS17(url, sStationName)
+    End Sub
+
+    Private Sub _StationHomepage(sender As Object, e As RoutedEventArgs)
+        Dim url As String = DirectCast(DirectCast(e.Source, MenuItem).CommandParameter, Searchresult).homepage
+        Process.Start(url)
+    End Sub
+
+    Private Sub _AddToFS17(url As String, ByVal sName As String)
+        Dim sFilename As String = "streamingInternetRadios.xml"
+        Dim sDirectory As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\my games\FarmingSimulator2017\music"
+        Dim sFullpath = Path.Combine(sDirectory, sFilename)
+        If File.Exists(sFullpath) Then
+            Try
+                Dim doc As XDocument = XDocument.Load(sFullpath)
+                Dim root As New XElement("streamingInternetRadio")
+                Dim comm As XComment = New XComment(sName)
+                root.Add(comm)
+                root.Add(New XAttribute("href", url))
+                doc.Element("streamingInternetRadios").Add(root)
+                doc.Save(sFullpath)
+                MsgBox($"{sName} successfully added.")
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    End Sub
+End Class
---
+++ FS17-RadiomanagerPro/Searchresult.vb
@@ -0,0 +1,191 @@
+﻿Public Class Searchresult
+    Public Property id() As String
+        Get
+            Return m_id
+        End Get
+        Set
+            m_id = Value
+        End Set
+    End Property
+    Private m_id As String
+    Public Property name() As String
+        Get
+            Return m_name
+        End Get
+        Set
+            m_name = Value
+        End Set
+    End Property
+    Private m_name As String
+    Public Property url() As String
+        Get
+            Return m_url
+        End Get
+        Set
+            m_url = Value
+        End Set
+    End Property
+    Private m_url As String
+    Public Property homepage() As String
+        Get
+            Return m_homepage
+        End Get
+        Set
+            m_homepage = Value
+        End Set
+    End Property
+    Private m_homepage As String
+    Public Property favicon() As String
+        Get
+            Return m_favicon
+        End Get
+        Set
+            m_favicon = Value
+        End Set
+    End Property
+    Private m_favicon As String
+    Public Property tags() As String
+        Get
+            Return m_tags
+        End Get
+        Set
+            m_tags = Value
+        End Set
+    End Property
+    Private m_tags As String
+    Public Property country() As String
+        Get
+            Return m_country
+        End Get
+        Set
+            m_country = Value
+        End Set
+    End Property
+    Private m_country As String
+    Public Property state() As String
+        Get
+            Return m_state
+        End Get
+        Set
+            m_state = Value
+        End Set
+    End Property
+    Private m_state As String
+    Public Property language() As String
+        Get
+            Return m_language
+        End Get
+        Set
+            m_language = Value
+        End Set
+    End Property
+    Private m_language As String
+    Public Property votes() As String
+        Get
+            Return m_votes
+        End Get
+        Set
+            m_votes = Value
+        End Set
+    End Property
+    Private m_votes As String
+    Public Property negativevotes() As String
+        Get
+            Return m_negativevotes
+        End Get
+        Set
+            m_negativevotes = Value
+        End Set
+    End Property
+    Private m_negativevotes As String
+    Public Property codec() As String
+        Get
+            Return m_codec
+        End Get
+        Set
+            m_codec = Value
+        End Set
+    End Property
+    Private m_codec As String
+    Public Property bitrate() As String
+        Get
+            Return m_bitrate
+        End Get
+        Set
+            m_bitrate = Value
+        End Set
+    End Property
+    Private m_bitrate As String
+    Public Property hls() As String
+        Get
+            Return m_hls
+        End Get
+        Set
+            m_hls = Value
+        End Set
+    End Property
+    Private m_hls As String
+    Public Property lastcheckok() As String
+        Get
+            Return m_lastcheckok
+        End Get
+        Set
+            m_lastcheckok = Value
+        End Set
+    End Property
+    Private m_lastcheckok As String
+    Public Property lastchecktime() As String
+        Get
+            Return m_lastchecktime
+        End Get
+        Set
+            m_lastchecktime = Value
+        End Set
+    End Property
+    Private m_lastchecktime As String
+    Public Property lastcheckoktime() As String
+        Get
+            Return m_lastcheckoktime
+        End Get
+        Set
+            m_lastcheckoktime = Value
+        End Set
+    End Property
+    Private m_lastcheckoktime As String
+    Public Property clicktimestamp() As String
+        Get
+            Return m_clicktimestamp
+        End Get
+        Set
+            m_clicktimestamp = Value
+        End Set
+    End Property
+    Private m_clicktimestamp As String
+    Public Property clickcount() As String
+        Get
+            Return m_clickcount
+        End Get
+        Set
+            m_clickcount = Value
+        End Set
+    End Property
+    Private m_clickcount As String
+    Public Property clicktrend() As String
+        Get
+            Return m_clicktrend
+        End Get
+        Set
+            m_clicktrend = Value
+        End Set
+    End Property
+    Private m_clicktrend As String
+    Public Property lastchangetime() As String
+        Get
+            Return m_lastchangetime
+        End Get
+        Set
+            m_lastchangetime = Value
+        End Set
+    End Property
+    Private m_lastchangetime As String
+End Class
